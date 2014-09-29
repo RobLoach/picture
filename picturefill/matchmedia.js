2c282f3d00a003ad7afc826ba8068e473d4a021e
jelles <sebreghts.jelle@gmail.com>
2014-09-29 15:58:38 +0200

    Issue #2333175 by rocket_nova: Fixed The addListener() polyfill in matchmedia.js is broken.

--- picturefill/matchmedia.js
+++ picturefill/matchmedia.js
@@ -11,6 +11,49 @@
  * polling to trigger registered listeners on matchMedia tests.
  * This polyfill triggers tests on window resize and orientationchange.
  */
+(function(Drupal) {
+  /**
+   * Adapted from underscore.js with the addition Drupal namespace.
+   *
+   * The debounce function wrapper should be used sparingly. One clear use case
+   * is limiting the invocation of a callback attached to the window resize event.
+   *
+   * Before using the debounce function wrapper, consider first whether the
+   * callback could be attached to an event that fires less frequently or if the
+   * function can be written in such a way that it is only invoked under specific
+   * conditions.
+   *
+   * @summary Limits the invocations of a function in a given time frame.
+   * @param {Function} func
+   *   The function to be invoked.
+   * @param {Number} wait
+   *   The time period within which the callback function should only be
+   *   invoked once. For example if the wait period is 250ms, then the callback
+   *   will only be called at most 4 times per second.
+   * @param {Boolean} [immediate=undefined]
+   *   Run the callback at the beginning.
+   */
+  Drupal.debounce = Drupal.decounce || function (func, wait, immediate) {
+    var timeout, result;
+    return function () {
+      var context = this;
+      var args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) {
+          result = func.apply(context, args);
+        }
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        result = func.apply(context, args);
+      }
+      return result;
+    };
+  };
+})(Drupal);
 
 window.matchMedia = window.matchMedia || (function (doc, window, Drupal) {
 
